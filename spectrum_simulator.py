e2ed6014474a03f3edc0df816a980cd2a1f1e259
alehono <hono.alexandre@gmail.com>
2024-10-25 09:45:14 -0300

    normalization

--- spectrum_simulator.py
+++ spectrum_simulator.py
@@ -56,7 +56,10 @@ def simulate_spectra_conditional(foldername, filename):
         "Aqui os espectros vão precisar ser calculados e salvos em uma tabela (usando append)"
         peakshiftparams = [par20c[0], tempparam[1], par20c[2], par20c[3], tempparam[4], par20c[5]]
         peakshiftspectrum = simulate_spectrum(x, peakshiftparams)
-        peakshiftspectra.append(peakshiftspectrum)
+        "Precisa renormalizar o espectro"
+        peakmax = np.max(peakshiftspectrum) # Encontrando o máximo
+        norm_peakshiftspectrum = peakshiftspectrum / peakmax # Normalizando a partir do máximo
+        peakshiftspectra.append(norm_peakshiftspectrum)
         peak = f"{peakshiftparams[1]}-{temp}"
         headerpeak.append(peak)
         
@@ -64,7 +67,10 @@ def simulate_spectra_conditional(foldername, filename):
         "Aqui os espectros vão precisar ser calculados e salvos em uma tabela (usando append)"
         fwhmchangeparams = [par20c[0], par20c[1], tempparam[2], par20c[3], par20c[4], tempparam[5]]
         fwhmchangespectrum = simulate_spectrum(x, fwhmchangeparams)
-        fwhmchangespectra.append(fwhmchangespectrum)
+        "Precisa renormalizar o espectro"
+        peakmax = np.max(fwhmchangespectrum) # Encontrando o máximo
+        norm_fwhmchangespectrum = fwhmchangespectrum / peakmax # Normalizando a partir do máximo
+        fwhmchangespectra.append(norm_fwhmchangespectrum)
         fwhm = f"{peakshiftparams[2]}-{temp}"
         headerfwhm.append(fwhm)
     
